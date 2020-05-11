e093fecc2cb1e6c7cf56aa9aa0f033711cc18e23
infoxicator <ruben@infoxication.net>
2020-05-12 00:22:14 +0100

    feat(): deploy action

--- src/duck.js
+++ src/duck.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux-immutable';
-import { resourcesReducer } from 'iguazu-rest';
 import { proceduresReducer } from 'iguazu-rpc';
 
 export default combineReducers({
-  resources: resourcesReducer,
   procedures: proceduresReducer,
 });
